271a75842488cb51d123b1ca17d7480c258a89c0
Arthur Riechert <riechertarthur@gmail.com>
2023-06-07 05:45:28 -0400

    Refactored

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from ghost.manager import GhostManager
 from cli import cli
+from llm.openai.model import OpenAIModel
 
 if __name__ == "__main__":
     
-    # ghost_manager = GhostManager()
+    ghost_manager = GhostManager()
+    
+    openai_model = OpenAIModel() 
+
     cli()
