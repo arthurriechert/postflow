2ac5eb97822ab0488d2114a73844b3b5fb2b5d2d
Arthur Riechert <riechertarthur@gmail.com>
2023-06-07 12:00:15 -0400

    Moved settings access to specialized file

--- src/cli.py
+++ src/cli.py
@@ -1,70 +1,4 @@
-import json
-
-import os
-
-def parse_settings(option: str, settings: dict):
-    """
-    Finds the correct setting value
-
-    Args:
-        option (str): The setting to find
-        settings (dict): Contains a dictionary with the settings
-
-    Returns:
-        value (var): Associated value for option
-        None: Returns None if can't find setting
-
-    """
-    
-    # Iterate over settings
-    for key, value in settings.items():
-        
-        if key == option:
-            return value
-    
-    return None
-
-def get_settings():
-    """
-    Loads settings from a json or creates json if it doesn't exist
-
-    Returns:
-        settings (dict): Dictionary containing the settings
-
-    """
-
-    # Create placeholder for settings
-    settings = {}
-
-    # Check if there is a settings file
-    if not os.path.exists("settings.json"):
-
-        # Print diagnostics
-        print(f"\n\033[31mNo settings detected\033[0m\n\033[32mCreating settings.json\033[0m")
-        # Define default settings
-        settings = {
-            "user": None,    
-            "temperature": 0.5,
-            "max_tokens": 2048,
-        }
-
-        # Save the json
-        with open("settings.json", "w") as file:
-            json.dump(settings, file)
-
-    else:
-        
-        # Print diagnostics
-        print(f"\n\033[33mLoading settings\033[0m")
-
-        # Load the json
-        with open("settings.json", "r") as file:
-            settings = json.load(file)
-
-        # Print diagnostics
-        print(f"\n\033[32mSettings loaded\033[0m")
-
-    return settings
+from settings import get_settings, parse_settings
 
 def display_settings(settings: dict):
     """
@@ -90,7 +24,8 @@ def display_settings(settings: dict):
     Select the letter of the option you choose.
 
     ############# OPTIONS ############
-    a) Edit Settings         b) Exit 
+    a) Edit Settings         b) Exit
+    
     > """)
 
     return choice
@@ -105,7 +40,7 @@ def cli():
     settings = get_settings()
 
     # Get name
-    name = parse_settings("user", settings)
+    name = parse_settings(settings, "user")
 
     # Keeps loop running
     running = True
@@ -126,12 +61,17 @@ def cli():
             ############# OPTIONS ############
 
             1) Settings
-            
+           
+            2) End Session
             > """))
 
             # View settings
             if choice == 1:
                 display_settings(settings)
+
+            # End session
+            elif choice == 2:
+                running = False
 
         except:
 
