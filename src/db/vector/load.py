815840d6c4dc680dae3cbf3e57f9ceac7b8d9317
Arthur Riechert <riechertarthur@gmail.com>
2023-06-10 23:02:39 -0400

    Added function to load saved index

--- src/db/vector/load.py
+++ src/db/vector/load.py
@@ -4,6 +4,9 @@ import faiss
 # Manipulating vectors
 import numpy as np
 
+# Checking files
+import os
+
 def create_new_db(vectors, path:str):
     """
     Creates a new faiss index
@@ -21,10 +24,10 @@ def create_new_db(vectors, path:str):
     vectors = np.vstack(vectors)
 
     # Measure number of features
-    features = np.shape[1]
+    features = vectors.shape[1]
 
     # Initialize FAISS index
-    index = faise.IndexFlatL2(features)
+    index = faiss.IndexFlatL2(features)
 
     # Add the vectors
     index.add(vectors)
@@ -34,3 +37,53 @@ def create_new_db(vectors, path:str):
 
     # Print diagnostics
     print(f"\nCreated new FAISS index at path: {path} and with {index.ntotal} entries.")
+
+
+def db_exists(path: str):
+    """
+    Checks if a database exists
+    
+    Args:
+        path (str): File path to the FAISS index
+
+    Returns:
+        (bool): If there is a index
+
+    """
+
+    # Look for the path
+    if os.path.exists(path):
+        return True
+    else:
+        return False
+
+
+def load_db(path: str):
+    """
+    Loads a FAISS index from file
+
+    Args:
+        path (str): The path for a storing an index
+
+    Returns:
+        index: FAISS index
+        exists (bool): True when index exists
+
+    """
+
+    # Determine if the path exists
+    if (db_exists(path)):
+
+        # Read file
+        index = faiss.read_index(path)
+        
+        # Print diagnostics
+        print("\n\033[32mIndex located and loads\033[0m")
+
+        return index, True
+
+    else:
+
+        return "Argggh! You flubbed up", False
+        
+        
