cdf03412a28baae26859cf14b63f580b68c40e9d
Arthur Riechert <riechertarthur@gmail.com>
2023-06-10 22:21:13 -0400

    Implemented vector db creation with faiss

--- src/db/vector/load.py
+++ src/db/vector/load.py
@@ -1,1 +1,36 @@
+# Manage vector db
 import faiss
+
+# Manipulating vectors
+import numpy as np
+
+def create_new_db(vectors, path:str):
+    """
+    Creates a new faiss index
+
+    Args:
+        vector (list): A list of vectors to add to an index
+        path (str): The path for storing the index
+
+    Returns:
+        None
+
+    """
+
+    # Convert to 2-D np array
+    vectors = np.vstack(vectors)
+
+    # Measure number of features
+    features = np.shape[1]
+
+    # Initialize FAISS index
+    index = faise.IndexFlatL2(features)
+
+    # Add the vectors
+    index.add(vectors)
+
+    # Store in a file
+    faiss.write_index(index, path)
+
+    # Print diagnostics
+    print(f"\nCreated new FAISS index at path: {path} and with {index.ntotal} entries.")
