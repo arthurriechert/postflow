51b3b0dce399c8efa8ae8b8d844733c1759031c1
Arthur Riechert <riechertarthur@gmail.com>
2023-06-19 12:00:14 -0400

    Added comments

--- src/main.py
+++ src/main.py
@@ -8,13 +8,16 @@ from enum import Enum
 from stream import ConnectionManager 
 from starlette.websockets import WebSocketDisconnect
 
+# Set up FastAPI
 app = FastAPI()
 manager = ConnectionManager()
 
+# Only allow local client while testing
 origins = [
-    "http://localhost:5173",  # Replace with the origin of your frontend app
+    "http://localhost:5173",
 ]
 
+# Setup CORS    
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -24,25 +27,62 @@ app.add_middleware(
 )
 
 class Tools(str, Enum):
+    """
+    Used to check for appropriate tool when someone calls API
+
+    """
     blogger = "blogger"
 
 @app.get("/api/tools/blogger/gpt-4")
 async def openai_blog_update():
+    """
+    Attempts the process of generating a new blog post
+
+    Args:
+        None
+
+    Returns:
+        (dict): Status message
+
+    """
     await blogger.run_single_update(manager)
     await manager.broadcast("Starting")
     return {"status": "Update process started"}
 
 @app.get("/api/tools/chat/gpt-4")
 async def openai_chat_completion():
+    """
+    Gets chat completion based on chat history
+
+    * Note: Takes json body
+
+    Args:
+        None
+
+    Returns:
+        (dict): Status message
+
+    """
     return {"status": "Chat sent"}
 
-@app.get("/api/test")
-async def get_test_data() -> dict:
-    return {"func": "test", "message": '<h2>this is a test</h2>\n<p style="margin-top: -15px">I hope the test was successful</p>'}
-
+# CONSIDER REVISING THIS METHOD OF TRANSMITTING STATUSES
 @app.websocket("/api/status/stream")
 async def stream_status(websocket: WebSocket):
+    """
+    Transmits messages across websocket API to Client
+
+    Args:
+        websocket (WebSocket): Interface for using web sockets
+
+    Returns:
+        None
+    
+    """
+
+    # Connect to the websocket manager
     await manager.connect(websocket)
+
+    # Broadcast messages
     try:
         while True:
             data = await websocket.receive_text()
@@ -51,13 +91,29 @@ async def stream_status(websocket: WebSocket):
         manager.disconnect(websocket)
         await manager.broadcast("A client has disconnected")
 
-@app.get("/message/{message}")
+@app.get("/api/test/{message}")
 async def send_message(message: str):
+    """
+    Sends a test message over the websocket stream
+
+    Args:
+        message (str): The message you want to transmit
+
+    Returns:
+        (dict): A status message
+
+    """
+
+    # Send the message over websocket
     await manager.broadcast(message)
-    return {"message", "Message sent"}
+    
+    return {"status": "Message sent"}
 
 if __name__ == "__main__":
+    """
+    Run the CLI
 
-    blogger.run_single_update() 
+    """
 
+    # Starts the command line interface
     cli()
